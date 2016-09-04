0d53fe00196049811fced84ad64b42c26231ce75
Mateus Ortiz <mateus.prado@ig.com.br>
2016-09-04 18:54:06 -0300

    Adds ServiceWorkers

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('pwaready').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                'index.html?homescreen=1',
+                '/?homescreen=1',
+                '/styles/styles.css'
+            ])
+            .then(() => self.skipWaiting())
+        })
+    )
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    )
+});
